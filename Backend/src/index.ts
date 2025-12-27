3a2660dfe8fb66d02720b08f5687f29954c125e7
mbethi <mbethi@revasolutions.com>
2025-12-27 14:06:23 +0530

    allowed all cors for testing

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -100,7 +100,7 @@ const app = express();
 const allowedOrigins = process.env.CORS?.split(",");
 app.use(
   cors({
-    origin: allowedOrigins, // Explicitly allow frontend URL
+    origin: "*", // Explicitly allow frontend URL
     credentials: true, // Allow cookies and authentication headers
   })
 );
@@ -129,7 +129,7 @@ app.use("/auth", router);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: allowedOrigins,
+    origin: "*",
     credentials: true,
   },
   transports: ["websocket"]
